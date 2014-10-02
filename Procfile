0bfda910872e87ce942a58f6d58f087704ec2797
Danny Archer <Danny.Archer@records.nsw.gov.au>
2014-10-03 08:07:38 +1000

    Adding web xml class

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 default_process_types:
-	web: java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/photo-inschmestigator.jar
+	web: java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/photo-inschmestigator.war
---
+++ src/main/groovy/photoInschmestigator/ApplicationWebXml.groovy
@@ -0,0 +1,18 @@
+package photoInschmestigator
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.web.SpringBootServletInitializer
+
+public class ApplicationWebXml extends SpringBootServletInitializer {
+
+    private final Logger log = LoggerFactory.getLogger(ApplicationWebXml.class)
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application
+                .showBanner(false)
+                .sources(Application.class)
+    }
+}
